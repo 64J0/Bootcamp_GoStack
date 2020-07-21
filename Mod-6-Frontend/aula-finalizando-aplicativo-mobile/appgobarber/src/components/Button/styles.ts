f09be8d21f7addd6184b4ead72f84c7df0d2d23c
Vinicius Gajo <vinigaio97@gmail.com>
2020-07-21 02:32:43 -0300

    Agendamento

--- Mod-6-Frontend/aula-finalizando-aplicativo-mobile/appgobarber/src/components/Button/styles.ts
+++ Mod-6-Frontend/aula-finalizando-aplicativo-mobile/appgobarber/src/components/Button/styles.ts
@@ -2,8 +2,6 @@ import styled from 'styled-components/native';
 import { RectButton } from 'react-native-gesture-handler';
 
 export const Container = styled(RectButton)`
-  display: flex;
-  width: 100%;
   height: 60px;
   background: #ff9000;
   border-radius: 10px;
